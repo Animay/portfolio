db24edceb4fe6233f271faae82db784b1c86dc14
unknown <animeshshr@gmail.com>
2017-12-18 01:51:45 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var scroll_start = 0;
+	var startchange = $('#about');
+	var offset = startchange.offset();
+	$(document).scroll(function() {
+		scroll_start = $(this).scrollTop();
+		if(scroll_start>offset.top)
+	}
+
+}
